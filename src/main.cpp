11ae1a373f354a806bc871f93ba37b2714383033
gavintryz <gavintryz@gmail.com>
2021-08-31 19:50:42 -0400

    main.cpp -> main.cpp.old3, replaced by Test's main

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,38 @@
 #include <Arduino.h>
 #include <PS4Controller.h>
+/*Example sketch to control a stepper motor with A4988/DRV8825 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
 
+// Define stepper motor connections and steps per revolution:
+#define R_DIR 14
+#define R_STEP 27
+#define L_DIR 32
+#define L_STEP 26
+#define stepsPerRevolution 200
+#define DEADZONE_SIZE 15
+#define MAX_STEP_DELAY 2500 // Max microseconds per step (slowest speed)
+#define MIN_STEP_DELAY 300 // Min microseconds per step (fastest speed)
+#define MOTOR_R_DIRECTION 1
+#define MOTOR_L_DIRECTION 1
+
+int delayTime = 500;
+int dir = 0;
 
 TaskHandle_t Rstep;
 TaskHandle_t Lstep;
 void WriteStepsL(void *pvParameters);
 void WriteStepsR(void *pvParameters);
-
-#define MAX_STEP_SPEED 1000 // Max steps per second
-#define MIN_STEP_SPEED 500 // Min steps per second
-#define STEPPER_ACCEL 200 // Max steps per second per second
-
-#define DEADZONE_SIZE 15
-
-#define MOTOR_R_DIRECTION 1 // Set to -1 to reverse motor direction, instead of rewiring
-#define MOTOR_L_DIRECTION 1 
-
-#define DIR_R 14 // Direction pin for Right motors
-#define DIR_L 32 // Direction pin for Left motors
-#define STEP_R 27 // Step pin for Right motors
-#define STEP_L 26 // Step pin for Left motors
-
-#define LED_CHANNEL_A 0
-#define LED_CHANNEL_B 1
-
-int leftStickY;
-int leftStepSpeed;
-
-int rightStickY;
-int rightStepSpeed;
+void someFunc(int isRight, int stepPin, int dirPin);
 
 
-void setup()
-{
-  
-  delay(1000);
-  
-  // Connect PS4 controller
-  Serial.begin(115200);
-  char add[] = "01:02:03:04:05:06";
-  PS4.begin(add);
+void setup() {
+  // Declare pins as output:
+  pinMode(R_DIR, OUTPUT);
+  pinMode(R_STEP, OUTPUT);
+  pinMode(L_DIR, OUTPUT);
+  pinMode(L_STEP, OUTPUT);
+    Serial.begin(115200);
+  char bluetoothMAC[] = "01:02:03:04:05:06";
+  PS4.begin(bluetoothMAC);
 
   xTaskCreatePinnedToCore(
         WriteStepsR, 
@@ -58,145 +51,64 @@ void setup()
         1,
         &Lstep,
         1);
+}
+
+void loop()
+{
 
 }
 
 void WriteStepsL(void *pvParameters)
 {
-  for(;;)
-  {
-    if(PS4.isConnected())
-    {
-   
-      // Get raw y values from left stick
-      leftStickY = PS4.data.analog.stick.ly;
-    
-      Serial.print("LStick: ");
-      Serial.print(leftStickY, DEC);
-
-      // Prevent values from being -128
-      if (leftStickY < 0)
-        leftStickY = leftStickY + 1;
-
-   
-      // Establish motor directions
-      // "* MOTOR_X_DIRECTION" allows motor to be reversed through code instead of wiring
-
-      if (leftStickY * MOTOR_L_DIRECTION < 0)
-      {
-         // Direction A (Clockwise)
-        digitalWrite(DIR_L, HIGH);
-      }
-   
-      else
-      {
-        // Direction B (Counterclockwise)
-        digitalWrite(DIR_L, LOW);
-      }
-
-      // Convert stick position to magnitude
-      leftStickY = abs(leftStickY);
-
-      // Map stick positions to stepper speed
-      leftStepSpeed = map(leftStickY, DEADZONE_SIZE, 127, MAX_STEP_SPEED, MIN_STEP_SPEED);
-   
-      // Overwrite if stick is within deadzone
-      if (leftStickY < DEADZONE_SIZE)
-        leftStepSpeed = 0;
-
-      if (rightStickY < DEADZONE_SIZE)
-        rightStepSpeed = 0;
-
-    
-      // Write stepper speeds to motors
-      if(leftStepSpeed != 0)
-      {  
-        digitalWrite(STEP_L,HIGH);
-        delayMicroseconds(leftStepSpeed);
-        digitalWrite(STEP_L, LOW);
-        delayMicroseconds(leftStepSpeed);
-
-      }
-
-      Serial.print("\tLeft Steps/Sec:\t");
-      Serial.print(leftStepSpeed);
-    }
-
-    else
-    {
-      Serial.println("Controller not connected...");
-      delay(1000);
-    }
-  }
+  someFunc(0, L_STEP, L_DIR);
 }
 
 void WriteStepsR(void *pvParameters)
 {
-  for(;;)
-  {
-    if(PS4.isConnected())
-    {
-   
-      // Get raw y values from left stick
-      rightStickY = PS4.data.analog.stick.ry;
-    
-      Serial.print("RStick: ");
-      Serial.print(rightStickY, DEC);
-
-      // Prevent values from being -128
-      if (rightStickY < 0)
-        rightStickY = rightStickY + 1;
-
-   
-      // Establish motor directions
-      // "* MOTOR_X_DIRECTION" allows motor to be reversed through code instead of wiring
-
-      if (rightStickY * MOTOR_L_DIRECTION < 0)
-      {
-         // Direction A (Clockwise)
-        digitalWrite(DIR_R, HIGH);
-      }
-   
-      else
-      {
-        // Direction B (Counterclockwise)
-        digitalWrite(DIR_R, LOW);
-      }
-
-      // Convert stick position to magnitude
-      rightStickY = abs(rightStickY);
-
-      // Map stick positions to stepper speed
-      rightStepSpeed = map(rightStickY, DEADZONE_SIZE, 127, 0, MIN_STEP_SPEED);
-   
-      // Overwrite if stick is within deadzone
-      if (rightStickY < DEADZONE_SIZE)
-        rightStepSpeed = 0;
-
-    
-      // Write stepper speeds to motors
-      if(rightStepSpeed != 0)
-      {  
-        digitalWrite(STEP_L,HIGH);
-        delayMicroseconds(rightStepSpeed);
-        digitalWrite(STEP_L, LOW);
-        delayMicroseconds(rightStepSpeed);
-
-      }
-
-      Serial.print("\tLeft Steps/Sec:\t");
-      Serial.print(leftStepSpeed);
-    }
-
-    else
-    {
-      Serial.println("Controller not connected...");
-      delay(1000);
-    }
-  }
+  someFunc(1, R_STEP, R_DIR);
 }
 
-void loop()
+void someFunc(int isRight, int stepPin, int dirPin)
 {
+  while(1)
+  {
+    if(!PS4.isConnected())
+    {
+      Serial.print("ps4 machine broke");
+      delay(100);
+    }
+    else
+    {
+      int stickVal = 0;
+      if(isRight)
+        stickVal = PS4.data.analog.stick.ry;
+      else
+        stickVal = PS4.data.analog.stick.ly;
+
+      if (stickVal * (isRight ? MOTOR_R_DIRECTION : MOTOR_L_DIRECTION) > 0)
+        digitalWrite(dirPin, HIGH);
+      else
+        digitalWrite(dirPin, LOW);
+
+      if (stickVal < 0)
+         stickVal = (int) abs(stickVal + 1);
+      
+    //   // Calculate deadzone
+      if (stickVal < DEADZONE_SIZE)
+         stickVal = 0;
+
+    //   // Map stick magnitudes to stepper delays (high stick value -> low delay, low stick value -> high delay)
+      delayTime = map(max(stickVal, DEADZONE_SIZE), DEADZONE_SIZE, 128, MAX_STEP_DELAY, MIN_STEP_DELAY);
+      if(stickVal != 0)
+      {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(delayTime);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(delayTime);
+      }
+      
+    }
+    vTaskDelay(1);
+  }
   
 }
